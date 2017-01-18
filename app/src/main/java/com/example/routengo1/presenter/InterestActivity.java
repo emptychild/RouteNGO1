f5738cb4af137ab9118fdf6f5757cd7a0962fe97
Иван Благовестный <wdart1995@gmail.com>
2017-01-18 23:08:34 +0200

    sfghsfghsf

--- app/src/main/java/com/example/routengo1/presenter/InterestActivity.java
+++ app/src/main/java/com/example/routengo1/presenter/InterestActivity.java
@@ -2,14 +2,36 @@ package com.example.routengo1.presenter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.example.routengo1.R;
+import com.example.routengo1.adapter.ItemAdapter;
+import com.example.routengo1.model.Item;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class InterestActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interests);
+
+        recyclerView = (RecyclerView) findViewById(R.id.objective_view);
+
+        List<Item> items = Arrays.asList(
+                new Item("Достопримечательности", ""),
+                new Item("Красивые места", ""),
+                new Item("Шоппиг", ""),
+                new Item("Барный марафон", "")
+        );
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(new ItemAdapter(items));
     }
 }
