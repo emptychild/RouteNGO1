1229ec13370b7c9aff9595220b5e5fe8f02e0243
Иван Благовестный <wdart1995@gmail.com>
2017-01-19 12:20:53 +0200

    sfghsfghsf

--- app/src/main/java/com/example/routengo1/presenter/InterestActivity.java
+++ app/src/main/java/com/example/routengo1/presenter/InterestActivity.java
@@ -1,9 +1,12 @@
 package com.example.routengo1.presenter;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.routengo1.R;
 import com.example.routengo1.adapter.ItemAdapter;
@@ -15,13 +18,17 @@ import java.util.List;
 public class InterestActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interests);
 
+        button = (Button) findViewById(R.id.i_ok_btn);
         recyclerView = (RecyclerView) findViewById(R.id.interest_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         List<Item> items = Arrays.asList(
                 new Item("Изобразительное искусство", ""),
@@ -30,9 +37,14 @@ public class InterestActivity extends AppCompatActivity {
                 new Item("История", ""),
                 new Item("Наука", "")
         );
-
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new ItemAdapter(items));
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(InterestActivity.this, ResultActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/routengo1/presenter/ProximityActivity.java
+++ app/src/main/java/com/example/routengo1/presenter/ProximityActivity.java
@@ -1,15 +1,29 @@
 package com.example.routengo1.presenter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.routengo1.R;
 
 public class ProximityActivity extends AppCompatActivity {
 
+    Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_proximity);
+
+        button = (Button) findViewById(R.id.p_ok_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ProximityActivity.this, InterestActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
