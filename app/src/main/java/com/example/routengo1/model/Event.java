0ce36b1c932be05c8beec490f814aa50c0b8db76
Иван Благовестный <wdart1995@gmail.com>
2017-01-24 17:03:24 +0200

    sfghsfghsf

---
+++ app/src/main/java/com/example/routengo1/model/Event.java
@@ -0,0 +1,27 @@
+package com.example.routengo1.model;
+
+/**
+ * Created by megaman on 24.01.2017.
+ */
+
+public class Event {
+
+    private Place place;
+    private Festival festival;
+
+    public Place getPlace() {
+        return place;
+    }
+
+    public void setPlace(Place place) {
+        this.place = place;
+    }
+
+    public Festival getFestival() {
+        return festival;
+    }
+
+    public void setFestival(Festival festival) {
+        this.festival = festival;
+    }
+}
---
+++ app/src/main/java/com/example/routengo1/model/Festival.java
@@ -0,0 +1,20 @@
+package com.example.routengo1.model;
+
+import java.util.List;
+
+/**
+ * Created by megaman on 24.01.2017.
+ */
+
+public class Festival{
+
+    private List<Event> eventList;
+
+    public List<Event> getEventList() {
+        return eventList;
+    }
+
+    public void setEventList(List<Event> eventList) {
+        this.eventList = eventList;
+    }
+}
